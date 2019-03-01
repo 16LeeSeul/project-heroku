3a86a8cfa48dddf38ebd3f9b191261e1562865b8
YOUR NAME <YOUR@EMAIL.com>
2019-03-02 00:56:30 +0900

    123

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-
+import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -22,13 +22,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 #SECRET_KEY = 'lr=&wai2)cni$zk8#=uy37m@gdfexgo*%7fc%$@lf9y05s)jqm'
 
-import os
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'lr=&wai2)cni$zk8#=uy37m@gdfexgo*%7fc%$@lf9y05s)jqm')
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -141,6 +140,6 @@ MEDIA_URL = '/media/'
 # 파일에 대한 url 설계
 
 # Heroku: Update database configuration from $DATABASE_URL.
-import dj_database_url
+
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
